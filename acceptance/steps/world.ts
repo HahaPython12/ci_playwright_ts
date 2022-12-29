5d096a571883148dfe7b166ffba50e18d087edea
Khaled Youssef <khaled_youssef@web.de>
2022-12-29 17:23:36 +0100

    changed: world.ts with process.exitCode

--- acceptance/steps/world.ts
+++ acceptance/steps/world.ts
@@ -28,6 +28,7 @@ After(async function(Scenario) {
     if(Scenario.result!.status === Status.FAILED){
         await this.attach(await page.screenshot({path: `./Screenshots/${Scenario.pickle.name}.png`, fullPage: true}), "image/png");
         //process.exit();
+        process.exitCode = 1;
         // throw new Error(`step failed`);
     }
     await browser.close();
