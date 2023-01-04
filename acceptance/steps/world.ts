5bd7f239dfbb2bf9899f49b74e8cc694c3d5d580
Khaled Youssef <khaled_youssef@web.de>
2023-01-04 17:19:27 +0100

    add process.exit() in world

--- acceptance/steps/world.ts
+++ acceptance/steps/world.ts
@@ -27,7 +27,7 @@ Before(async () => {
 After(async function(Scenario) {
     if(Scenario.result!.status === Status.FAILED){
         await this.attach(await page.screenshot({path: `./Screenshots/${Scenario.pickle.name}.png`, fullPage: true}), "image/png");
-        // process.exit(1);
+        process.exit(1);
         //process.exitCode = 1;
         // throw new Error(`step failed`);
     }
